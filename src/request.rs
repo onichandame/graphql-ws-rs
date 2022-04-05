bfbc5441a301a58abaeb3906b5ccd506d1a1f06f
onichandame <zxinmyth@gmail.com>
2022-04-05 13:00:34 +0000

    chore: fix variables

--- src/request.rs
+++ src/request.rs
@@ -6,8 +6,19 @@ pub struct Request<TVariables = (), TExtensions = ()> {
     pub query: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub operation_name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub variables: Option<TVariables>,
+    pub variables: TVariables,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub extensions: Option<TExtensions>,
+}
+
+trait IsUnit<T> {
+    fn is_unit(&self) -> bool {
+        false
+    }
+}
+
+impl IsUnit<()> for () {
+    fn is_unit(&self) -> bool {
+        true
+    }
 }
